6b82db6c35c75b210d49f9005d0b05947e50f37e
Snake_PRG <62793663+SnakePRG@users.noreply.github.com>
2022-02-19 20:09:07 +0300

    baslat.py

--- baslat.py
+++ baslat.py
@@ -1,5 +1,18 @@
 import time
 import webbrowser
+import random
+
+def sifreol():
+    import random
+ 
+ 
+    def rastgele_sifre_uret(uzunluk):
+        harfler = 'abcdefghiABCDEFGHI123467890!_-*!_-*!_-*!_-*'
+        sonuc = ''.join(random.choice(harfler) for i in range(uzunluk))
+        return sonuc
+ 
+    print("Şifreniz :",rastgele_sifre_uret(8))
+ 
 
 def ortlm():
     snv1 = int(input("Birinci sınav sonucunu giriniz: "))
@@ -10,8 +23,7 @@ def ortlm():
     sonuc = ((snv1+snv2+snv3+snv4)/4)
 
     print("Ortalamanız :", sonuc)
-    time.sleep(2)
-    exit()
+    
 
 def hspmak():
     sy1 = int(input("Birinci sayıyı girin :"))
@@ -34,10 +46,11 @@ def hspmak():
 print("""
 1. Ortalama hesaplama
 2. Hesap makinesi
-3. SnakePRG web sitesi
-4. SnakePRG github adresi
-5. Python diğer program
-İşlemleri görmek için 'işlemler' ya da '6' ya basın.
+3. Şifre oluşturucu
+4. SnakePRG web sitesi
+5. SnakePRG github adresi
+6. Python diğer program 
+İşlemleri görmek için 'işlemler' ya da '7' ya basın.
 Çıkmak için 'q' ya basın.
 """)
 
@@ -56,29 +69,33 @@ while True:
         hspmak()
 
     elif islem == "3":
-        print("Açılıyor...")
-        webbrowser.open("https://eneserolsahin.wixsite.com/snakeprg")     
+        sifreol()
 
     elif islem == "4":
         print("Açılıyor...")
+        webbrowser.open("https://eneserolsahin.wixsite.com/snakeprg")     
+
+    elif islem == "5":
+        print("Açılıyor...")
         webbrowser.open("https://github.com/snakeprg")     
 
-    elif islem == "5": 
+    elif islem == "6": 
         print("Açılıyor...")
         webbrowser.open("https://github.com/SnakePRG/py-ornek/archive/refs/heads/main.zip")
         
 
-    elif islem == "6" or islem == "islem" or islem == "işlem" or islem == "işlemler" or islem == "islemler":
+    elif islem == "7" or islem == "islem" or islem == "işlem" or islem == "işlemler" or islem == "islemler":
         print("""
 1. Ortalama hesaplama
 2. Hesap makinesi
-3. SnakePRG web sitesi
-4. SnakePRG github web sitesi
-5. Python diğer program
-İşlemleri görmek için 'işlemler' ya da '6' ya basın.
+3. Şifre oluşturucu
+4. SnakePRG web sitesi
+5. SnakePRG github adresi
+6. Python diğer program 
+İşlemleri görmek için 'işlemler' ya da '7' ya basın.
 Çıkmak için 'q' ya basın.
         """)
     
     else:
-        print("Böyle bir işlem bulunmamakta, eğer eklenmesini istediğiniz bir işlem varsa '3' e basarak web sitemizden bize ulaşabilirsiniz.")
+        print("Böyle bir işlem bulunmamakta, eğer eklenmesini istediğiniz bir işlem varsa '4' e basarak web sitemizden bize ulaşabilirsiniz.")
         time.sleep(2)
